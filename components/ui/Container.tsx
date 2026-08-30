5bcc6d4b6346451543a222dd93d6e7bb646528d2
by-aadat <ashishjio110@gmail.com>
2026-08-30 17:44:46 +0530

    Delete components directory

--- components/ui/Container.tsx
+++
@@ -1,3 +0,0 @@
-export function Container({ children, className = "" }: { children: React.ReactNode; className?: string }) {
-  return <div className={`mx-auto w-full max-w-[1280px] px-4 sm:px-6 ${className}`}>{children}</div>;
-}
